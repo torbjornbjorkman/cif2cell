05b48d9a1331e790c419469e95ec8ed0fa669866
Torbjörn Björkman <torbjornb@gmail.com>
2013-03-13 06:03:29 +0000

    version 1.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ docfiles = ['docs/cif2cell.pdf']
 ciffiles = glob('cifs/*.cif')
 
 setup(name='cif2cell',
-      version='1.0.6',
+      version='1.0.7',
       description='Construct a unit cell from CIF data',
       long_description='A command-line tool to generate the geometrical setup for various electronic structure codes from a CIF format file.',
       author='Torbjorn Bjorkman',
