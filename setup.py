93a440210adbed8a877a5221c4b497eb9d158bec
Leopold Talirz <leopold.talirz@gmail.com>
2019-09-16 09:57:07 +0200

    modifications for packaging on PyPI
    remove pycifrw package, which is now a dependency
    
    Note: The code does not actually seem to be python3 compatible...
    this will be fixed in a different PR

--- setup.py
+++ setup.py
@@ -14,34 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with cif2cell.  If not, see <http://www.gnu.org/licenses/>.
 #
-from distutils.core import setup
+from setuptools import setup, find_packages
 from glob import glob
-from subprocess import call
-from os import remove
-import sys
-import shutil
-import tarfile
-
-# Check for PyCifRW and attempt to install bundled version if not available.
-pycifrwmodulelist = []
-try:
-    import CifFile
-except:
-    try:
-        print("PyCifRW not found. Installing relevant modules.")
-        pycifrwmodulelist = ['CifFile','StarFile','YappsStarParser_1_0','YappsStarParser_1_1',
-                             'YappsStarParser_DDLm','yapps3_compiled_rt']
-        tar = tarfile.open('PyCifRW-3.3.tar.gz')
-        tar.extractall()
-        for mod in pycifrwmodulelist:
-            shutil.copy("PyCifRW-3.3/"+mod+".py","./")
-        shutil.rmtree('PyCifRW-3.3')
-    except:
-        print("PyCifRW could neither be found nor installed. Please install PyCIFRW manually, "+\
-              "either from the\ncopy provided in this package or from: "+\
-              "https://sourceforge.net/projects/pycifrw.berlios/\n"+\
-              "(or maybe you have it installed and just need to adjust your PYTHONPATH).")
-        sys.exit(1)
 
 # Set up documentation
 docfiles = ['docs/cif2cell.pdf']
@@ -50,26 +24,22 @@ docfiles = ['docs/cif2cell.pdf']
 ciffiles = glob('cifs/*.cif')
 periodiccifs = glob('cifs/periodic_table/*.cif')+['cifs/periodic_table/README']
 
-# python modules
-modulelist = pycifrwmodulelist+['utils','uctools','spacegroupdata','elementdata','ESPInterfaces']
-
 setup(name='cif2cell',
       version='2.0.0',
       description='Construct a unit cell from CIF data',
-      long_description='A command-line tool to generate the geometrical setup for various electronic structure codes from a CIF format file.',
+      long_description=open('README').read(),
       author='Torbjorn Bjorkman',
       author_email='torbjornb@gmail.com',
       url='http://cif2cell.sourceforge.net/',
-      py_modules=modulelist,
-      scripts=['cif2cell'],
-      requires=['CifFile'],
+      scripts=['binaries/cif2cell', 'binaries/vasp2cif'],
+      install_requires=[
+          "PyCifRW==4.2.1; python_version < '3'",
+          "PyCifRW==4.4; python_version >= '3'",
+      ],
+      packages=find_packages(),
       data_files=[('lib/cif2cell', ['LICENSE','HOWTOCITE']),
                   ('lib/cif2cell/sample_cifs', ciffiles),
                   ('lib/cif2cell/sample_cifs/periodic_table', periodiccifs),
                   ('lib/cif2cell/docs',docfiles)],
       license='GNU General Public License version 3'
       )
-
-# cleanup
-for mod in pycifrwmodulelist:
-    remove("./"+mod+".py")
