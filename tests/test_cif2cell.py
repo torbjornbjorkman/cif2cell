4d93c7af3c716fbefefc0116d0d3eee696bfed8b
Leopold Talirz <leopold.talirz@gmail.com>
2019-10-07 17:27:31 +0200

    remove test script in favor of pytest
     * tests moved to pytest
     * simplified tests using parametrize
       + reduced code duplication

--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -1,37 +1,34 @@
-import unittest
-import subprocess
+from __future__ import absolute_import
 import glob
 import os
+import pytest
+import subprocess
 
+CIF_FILES = glob.glob('cifs/*.cif')
 
-class TestCif2Cell(unittest.TestCase):
-    def test_cif(self):
-        for path in glob.glob('cifs/*.cif'):
-            p = subprocess.Popen(['./binaries/cif2cell', path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            stdout_data, stderr_data = p.communicate()
-            print("OUT")
-            print(stdout_data.decode())
+def run_cif2cell(args):
+    return subprocess.check_output(['./binaries/cif2cell'] + args, stderr=subprocess.STDOUT).decode('utf8')
 
-            assert not "***Warning: Space group operation check failed" in stdout_data.decode()
-            assert not "Error" in stdout_data.decode()
+@pytest.mark.parametrize("cif_file", CIF_FILES)
+def test_parse(cif_file):
+    """Test running cif2cell on each CIF file in /cifs."""
+    result = run_cif2cell([cif_file])
 
-    def test_vasp(self):
-        # for path in glob.glob('cifs/*.cif'):
-        p = subprocess.Popen(['./binaries/cif2cell', "-p", "vasp", "-f", 'cifs/Si.cif'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout_data, stderr_data = p.communicate()
-        print("OUT")
-        print(stdout_data.decode())
+    assert not "***Warning: Space group operation check failed" in result
+    assert not "Error" in result
 
-        assert not "***Warning: Space group operation check failed" in stdout_data.decode()
-        assert not "Error" in stdout_data.decode()
-        assert os.path.exists("POSCAR")
+def test_vasp():
+    """Test VASP output."""
+    cif_file = "./cifs/Si.cif"
+    result = run_cif2cell(["-p", "vasp", "-f", cif_file])
+    assert not "***Warning: Space group operation check failed" in result
+    assert not "Error" in result
+    assert os.path.exists("POSCAR")
 
-    def test_castep(self):
-        # for path in glob.glob('cifs/*.cif'):
-        p = subprocess.Popen(['./binaries/cif2cell', "-p", "castep", "-f", 'cifs/Si.cif'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout_data, stderr_data = p.communicate()
-        print("OUT")
-        print(stdout_data.decode())
+def test_castep():
+    """Test CASTEP output."""
+    cif_file = "./cifs/Si.cif"
+    result = run_cif2cell(["-p", "castep", "-f", cif_file])
 
-        assert not "***Warning: Space group operation check failed" in stdout_data.decode()
-        assert not "Error" in stdout_data.decode()
+    assert not "***Warning: Space group operation check failed" in result
+    assert not "Error" in result
