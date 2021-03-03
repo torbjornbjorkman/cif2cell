9436e7d3fcede5f3e63a08adf9f5ad9e09911b96
Leopold Talirz <leopold.talirz@gmail.com>
2021-03-03 14:01:52 +0100

    switch to installed cif2cell script

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ ciffiles = glob('cifs/*.cif')
 periodiccifs = glob('cifs/periodic_table/*.cif')+['cifs/periodic_table/README']
 
 setup(name='cif2cell',
-      version='2.0.0a3',
+      version='2.0.0a4',
       description='Construct a unit cell from CIF data',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -5,10 +5,9 @@ from pathlib import Path
 TEST_DIR = Path(__file__).resolve().parent
 CIFS_DIR = TEST_DIR.parent / 'cifs'
 CIF_FILES = CIFS_DIR.glob('*.cif')
-CIF2CELL_SCRIPT = TEST_DIR.parent / 'binaries' / 'cif2cell'
 
 def run_cif2cell(args):
-    return subprocess.check_output([CIF2CELL_SCRIPT] + args, stderr=subprocess.STDOUT).decode('utf8')
+    return subprocess.check_output(["cif2cell"] + args, stderr=subprocess.STDOUT).decode('utf8')
 
 @pytest.mark.parametrize("cif_file", CIF_FILES)
 def test_parse(cif_file):
