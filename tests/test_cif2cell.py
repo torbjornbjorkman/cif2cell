b06bc6a4469a021dbaa9833e6a557b7708d884c4
Leopold Talirz <leopold.talirz@gmail.com>
2021-03-03 13:52:56 +0100

    fix tests

--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -1,24 +1,18 @@
-
-import glob
-import os
-import sys
 import pytest
 import subprocess
+from pathlib import Path
 
-TEST_DIR = os.path.dirname(os.path.realpath(__file__))
-CIFS_DIR = os.path.join( os.path.join(TEST_DIR, os.path.pardir, 'cifs') )
-CIF_FILES = glob.glob(os.path.join(CIFS_DIR, '*.cif'))
+TEST_DIR = Path(__file__).resolve().parent
+CIFS_DIR = TEST_DIR.parent / 'cifs'
+CIF_FILES = CIFS_DIR.glob('*.cif')
+CIF2CELL_SCRIPT = TEST_DIR.parent / 'binaries' / 'cif2cell'
 
 def run_cif2cell(args):
-    return subprocess.check_output(['./binaries/cif2cell'] + args, stderr=subprocess.STDOUT).decode('utf8')
+    return subprocess.check_output([CIF2CELL_SCRIPT] + args, stderr=subprocess.STDOUT).decode('utf8')
 
 @pytest.mark.parametrize("cif_file", CIF_FILES)
 def test_parse(cif_file):
     """Test running cif2cell on each CIF file in /cifs."""
-    if sys.version_info < (3,0) and 'SiC.cif' in cif_file:
-        pytest.skip(msg='skip test for files with unicode content under python 2.7.' +
-           'see https://github.com/torbjornbjorkman/cif2cell/issues/7')
-
     result = run_cif2cell([cif_file])
 
     assert not "***Warning: Space group operation check failed" in result
@@ -26,15 +20,15 @@ def test_parse(cif_file):
 
 def test_vasp():
     """Test VASP output."""
-    cif_file = os.path.join(CIFS_DIR, "Si.cif")
+    cif_file = CIFS_DIR / "Si.cif"
     result = run_cif2cell(["-p", "vasp", "-f", cif_file])
     assert not "***Warning: Space group operation check failed" in result
     assert not "Error" in result
-    assert os.path.exists("POSCAR")
+    assert Path("POSCAR").exists()
 
 def test_castep():
     """Test CASTEP output."""
-    cif_file = os.path.join(CIFS_DIR, "Si.cif")
+    cif_file = CIFS_DIR / "Si.cif"
     result = run_cif2cell(["-p", "castep", "-f", cif_file])
 
     assert not "***Warning: Space group operation check failed" in result
